20b2116ede7e86fa7e355c6f4d267eeedcec41bb
Jaehwi-So <sjh9708@gmail.com>
2024-04-20 15:27:47 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 596 ms, Memory: 54320 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -3,22 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 순회 -> 100억 -> X
 public class Main {
     static long[] numbers;
-    static int[][] range;
-    static int maxR = Integer.MIN_VALUE;
-    static int minR = Integer.MAX_VALUE;
-
-    static long[] dp;
-
-    static void calc(){
-        long sum = 0;
-        for(int i = minR; i <= maxR; i++){
-            sum += numbers[i];
-            dp[i] = sum;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,31 +12,22 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         numbers = new long[n + 1];
-        range = new int[m + 1][2];
-        dp = new long[n + 1];
 
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
-            numbers[i] = Long.parseLong(st.nextToken());
+            numbers[i] = numbers[i-1] + Long.parseLong(st.nextToken());
         }
-
-        for(int i = 1; i <= m; i++){
-            st = new StringTokenizer(br.readLine());
-            range[i][0] = Integer.parseInt(st.nextToken());
-            range[i][1] = Integer.parseInt(st.nextToken());
-            minR = Math.min(range[i][0], minR);
-            maxR = Math.max(range[i][1], maxR);
-        }
-        
-
-        calc();
-
 
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= m; i++){
-            long result = dp[range[i][1]] - dp[range[i][0]] + numbers[range[i][0]];
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            long result = numbers[end] - numbers[start - 1];
             sb.append(result).append("\n");
         }
+
         System.out.println(sb);
+
     }
 }
