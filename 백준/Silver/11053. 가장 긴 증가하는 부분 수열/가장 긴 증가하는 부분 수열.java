fa5409739c7d75e995b2b1013d74f02939969fde
Jaehwi-So <sjh9708@gmail.com>
2024-04-30 21:52:19 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,26 +1,19 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 0 : 3  X = 1
- * 1 : 5  dp[0] = 2
- * 2 : 7  dp[1] vs dp[0] = 3
- * 3 : 9  dp[0] vs dp[1] vs vs dp[2] = 4
- * 4 : 2  X = 1
- * 5 : 1  X = 1
- * 6 : 4  dp[5] vs dp[4] vs dp[0] = 2
- * 7 : 8  dp[6] vs dp[5] vs dp[4] vs dp[2] vs dp[1] vs dp[0] = 4
- */
+
 public class Main {
     static Integer[] numbers;
     static Integer[] dp;
 
+
     static Integer calc(int n){
         if(dp[n] == null){
             dp[n] = 1;
-            for(int i = n - 1; i >= 0; i--){
+            for(int i = n; i > 0; i--){
                 if(numbers[i] < numbers[n]){
                     dp[n] = Math.max(calc(i) + 1, dp[n]);
                 }
@@ -34,19 +27,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        numbers = new Integer[n];
-        dp = new Integer[n];
+        numbers = new Integer[n + 1];
+        dp = new Integer[n + 1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             calc(i);
         }
 
+
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             max = Math.max(dp[i], max);
         }
         System.out.println(max);
