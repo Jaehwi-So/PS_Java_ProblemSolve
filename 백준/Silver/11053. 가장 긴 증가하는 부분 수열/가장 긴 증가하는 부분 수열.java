8ebcd0b381c6a86956202279c66bdbacfa6c8ddf
Jaehwi-So <sjh9708@gmail.com>
2024-04-30 23:22:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,7 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
@@ -9,15 +9,40 @@ public class Main {
     static Integer[] numbers;
     static Integer[] dp;
 
+    static Integer[] dist;
+
+
+    static int binarySearch(int value, int left, int right){
+        if(left == right){
+            return left - 1;
+        }
+        else{
+            int mid = (left + right) / 2;
+            if(dist[mid] == value){
+                return mid - 1;
+            }
+            else if(dist[mid] > value){
+                return binarySearch(value, left, mid);
+            }
+            else{
+                return binarySearch(value, mid + 1, right);
+            }
+        }
+    }
+
+    static int binarySearch(int value){
+        return binarySearch(value, 1, dist.length - 1);
+    }
+
+
 
     static Integer calc(int n){
         if(dp[n] == null){
-            dp[n] = 1;
-            for(int i = n; i > 0; i--){
-                if(numbers[i] < numbers[n]){
-                    dp[n] = Math.max(calc(i) + 1, dp[n]);
-                }
-            }
+            int num = numbers[n];
+            int d = binarySearch(num);
+            int index = d + 1;
+            dp[n] = index;
+            dist[index] = Math.min(dist[index], num);
         }
 
         return dp[n];
@@ -29,10 +54,19 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         numbers = new Integer[n + 1];
         dp = new Integer[n + 1];
+        dist = new Integer[n + 1];
+
+
         st = new StringTokenizer(br.readLine());
+        dp[0] = 0;
+        numbers[0] = 0;
+
         for(int i = 1; i <= n; i++){
             numbers[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
 
         for(int i = 1; i <= n; i++){
             calc(i);
@@ -43,6 +77,8 @@ public class Main {
         for(int i = 1; i <= n; i++){
             max = Math.max(dp[i], max);
         }
+//        System.out.println(Arrays.toString(dp));
+//        System.out.println(Arrays.toString(dist));
         System.out.println(max);
     }
 }
