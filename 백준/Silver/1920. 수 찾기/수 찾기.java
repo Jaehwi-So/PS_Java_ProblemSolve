edf8060b1cb6a6805b3a2053073ee79944a335ba
Jaehwi-So <sjh9708@gmail.com>
2024-05-05 23:29:37 +0900

    [Silver IV] Title: 수 찾기, Time: 1388 ms, Memory: 53944 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int find(int[] array, int number){
-        int start = 0;
-        int end = array.length - 1;
-        while(start <= end){
-            int index = (end + start) / 2;
-            if(number == array[index]){
-                return 1;
-            }
-            else if(number > array[index]){
-                start = index + 1;
-            }
-            else{
-                end = index - 1;
-            }
-        }
-        return 0;
-    }
+//    static int find(int[] array, int number){
+//        int start = 0;
+//        int end = array.length - 1;
+//        while(start <= end){
+//            int mid = (end + start) / 2;
+//            if(number == array[mid]){
+//                return 1;
+//            }
+//            else if(number > array[mid]){
+//                start = mid + 1;
+//            }
+//            else{
+//                end = mid - 1;
+//            }
+//        }
+//        return 0;
+//    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,9 +32,8 @@ public class Main {
         for(int i = 0; i < n; i++){
             array[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(array);
 
-//        System.out.println(Arrays.toString(array));
+        Arrays.sort(array); // 정렬
 
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
@@ -42,7 +41,9 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++){
             int number = Integer.parseInt(st.nextToken());
-            System.out.println(find(array, number));
+            int index = Arrays.binarySearch(array, number);
+            int result = index < 0 ? 0 : 1;
+            System.out.println(result);
         }
 
     }
