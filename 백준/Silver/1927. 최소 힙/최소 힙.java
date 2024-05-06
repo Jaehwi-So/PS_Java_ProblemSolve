48e4e19be0d03b3b65f33ec854949da9bfd33b5c
Jaehwi-So <sjh9708@gmail.com>
2024-05-07 00:00:21 +0900

    [Silver II] Title: 최소 힙, Time: 384 ms, Memory: 31472 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            if(k == 0){
+                if(queue.isEmpty()){
+                    sb.append(0).append("\n");
+                }
+                else{
+                    sb.append(queue.poll()).append("\n");
+                }
+            }
+            else{
+                queue.offer(k);
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
