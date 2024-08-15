f0a79b09e6f23fb55ef4fbc9ca64f1c51e2ab56f
Jaehwi-So <sjh9708@gmail.com>
2024-08-16 00:57:43 +0900

    [Gold IV] Title: 알고스팟, Time: 124 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,8 +1,7 @@
-
 import java.io.*;
 import java.util.*;
 
-class Node{
+class Node implements Comparable<Node>{
     int row;
     int col;
     int dist;
@@ -11,32 +10,40 @@ class Node{
         this.col = col;
         this.dist = dist;
     }
+
+    public int compareTo(Node n){
+        return this.dist - n.dist;
+    }
 }
+
 public class Main {
     static int n;
     static int m;
     static char[][] matrix;
-    static int[][] dp;
+    static int[][] distance;
+    static boolean[][] visited;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
-    static void bfs(){
+    static void djakstra(){
         Node start = new Node(0, 0, 0);
-        Queue<Node> queue = new LinkedList<>();
+        PriorityQueue<Node> queue = new PriorityQueue<>();
         queue.offer(start);
-        dp[0][0] = Character.getNumericValue(matrix[0][0]);
+        distance[0][0] = 0;
 
         while(!queue.isEmpty()){
             Node current = queue.poll();
-            if(dp[current.row][current.col] >= current.dist){
-                for(int i = 0; i < 4; i++){
-                    int nrow = current.row + dx[i];
-                    int ncol = current.col + dy[i];
-                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
-                        int nextDist = current.dist + Character.getNumericValue(matrix[nrow][ncol]);
-                        if(nextDist < dp[nrow][ncol]){
-                            dp[nrow][ncol] = nextDist;
-                            queue.offer(new Node(nrow, ncol, nextDist));
-                        }
+            if(visited[current.row][current.col]){
+                continue;
+            }
+            visited[current.row][current.col] = true;
+            for(int i = 0; i < 4; i++){
+                int nrow = current.row + dx[i];
+                int ncol = current.col + dy[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
+                    int nextDist = distance[current.row][current.col] + Character.getNumericValue(matrix[nrow][ncol]);
+                    if(nextDist < distance[nrow][ncol]){
+                        distance[nrow][ncol] = nextDist;
+                        queue.offer(new Node(nrow, ncol, nextDist));
                     }
                 }
             }
@@ -48,21 +55,21 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         matrix = new char[n][m];
-        dp = new int[n][m];
-        for(int[] line : dp){
+        distance = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int[] line : distance){
             Arrays.fill(line, Integer.MAX_VALUE);
         }
+
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             matrix[i] = st.nextToken().toCharArray();
         }
 
-        bfs();
+        djakstra();
 
-//        for(int[] line : dp){
-//            System.out.println(Arrays.toString(line));
-//        }
-        System.out.println(dp[n-1][m-1]);
+        System.out.println(distance[n-1][m-1]);
 
     }
 }
