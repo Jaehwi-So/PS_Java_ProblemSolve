6802b8188aa4092cb620cfa5320817150a473955
Jaehwi-So <sjh9708@gmail.com>
2024-08-19 07:35:13 +0900

    [level 3] Title: 네트워크, Time: 0.94 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -21,8 +21,33 @@ class Solution {
         }
     }
     
+    
+    // Union-find
+    static int[] parent;
+    
+    static void union(int n1, int n2){
+        int k1 = find(n1);
+        int k2 = find(n2);
+        if(k1 > k2){
+            parent[k1] = k2;
+        }
+        else if(k2 > k1){
+            parent[k2] = k1;
+        }
+    }
+    
+    static int find(int n1){
+        if(parent[n1] != n1){
+            parent[n1] = find(parent[n1]);
+        }
+        return parent[n1];
+        
+    }
+    
     public int solution(int n, int[][] computers) {
         this.n = n;
+        
+        // 1. BFS
         matrix = computers;
         visited = new boolean[n];
         int answer = 0;
@@ -33,6 +58,27 @@ class Solution {
                 answer++;
             }
         }
+        
+        // 2. Union-find
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && computers[i][j] == 1){
+                    union(i, j);
+                }
+            }
+        }
+        
+        Set<Integer> set = new HashSet();
+        for(int i = 0; i < n; i++){
+            set.add(find(i));
+        }
+        answer = set.size();
         
         return answer;
     }
