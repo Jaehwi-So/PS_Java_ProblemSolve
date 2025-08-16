e4aff45d28a73a65375504a826601b61d67974eb
Jaehwi-So <sjh9708@gmail.com>
2025-08-17 01:31:02 +0900

    [Platinum V] Title: 주유소, Time: 304 ms, Memory: 67416 KB, Score: 73 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.java
+++ 백준/Platinum/13308. 주유소/주유소.java
@@ -5,33 +5,34 @@ public class Main {
     static int n, m;
     static int[] costs;
     static List<int[]>[] graph;
-    static final int MAX = 40000 * 2500 * 2500;
+    static final long MAX = 4000L * 2500L * 2500L;
 
-    static int dijakstra(){
+    static long dijakstra(){
 
-        int[][] visited = new int[n+1][2501];
-        for(int[] line : visited){
+        long[][] visited = new long[n+1][2501];
+        for(long[] line : visited){
             Arrays.fill(line, MAX);
         }
-        Queue<int[]> queue = new LinkedList<>(); //노드, 총비용, 최소기름비용
-        queue.offer(new int[]{1, 0, costs[1]});
+        Queue<long[]> queue = new LinkedList<>(); //노드, 총비용, 최소기름비용
+        queue.offer(new long[]{1, 0, costs[1]});
         visited[1][costs[1]] = 0;
 
         while(!queue.isEmpty()){
-            int[] current = queue.poll();
-            if(visited[current[0]][current[2]] < current[1]) continue;
+            long[] current = queue.poll();
+//            System.out.println(current[0] + " " + current[2] + " : " + current[1]);
+            if(visited[(int)current[0]][(int)current[2]] < current[1]) continue;
 
-            for(int[] next : graph[current[0]]){
-                int val = current[1] + (current[2] * next[1]);
-                int min = Math.min(costs[next[0]], current[2]);
+            for(int[] next : graph[(int)current[0]]){
+                long val = current[1] + (current[2] * next[1]);
+                int min = Math.min(costs[next[0]], (int)current[2]);
                 if(visited[next[0]][min] > val){
                     visited[next[0]][min] = val;
-                    queue.offer(new int[]{next[0], val, min});
+                    queue.offer(new long[]{next[0], val, min});
                 }
             }
         }
 
-        int result = MAX;
+        long result = MAX;
         for(int i = 0; i <= 2500; i++){
             result = Math.min(result, visited[n][i]);
         }
